24aa63acd2f0951f8345e50193f02d4c27c6bcbb
mkbonini <mkbonini@gmail.com>
2022-08-04 16:20:25 -0600

    feat: discounts page links to discount show page

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -2,4 +2,8 @@ class MerchantDiscountsController < ApplicationController
     def index
         @merchant = Merchant.find(params[:merchant_id])
     end
+
+    def show
+        @discount = BulkDiscount.find(params[:id])
+    end
 end
