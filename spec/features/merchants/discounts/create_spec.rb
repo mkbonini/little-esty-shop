c1e87c781495ecddd0bd3c40d7cbfba9e3532b53
mkbonini <mkbonini@gmail.com>
2022-08-04 17:03:43 -0600

    test: sad path test for new discount

--- spec/features/merchants/discounts/create_spec.rb
+++ spec/features/merchants/discounts/create_spec.rb
@@ -17,4 +17,17 @@ RSpec.describe 'Merchant Discounts Create' do
             expect(page).to have_link("Threshold Quantity: 10, Discount: 50%")
         end
     end
+
+    it 'goes back to form with an error message if not all fields are filled' do
+        merchant = Merchant.create!(name: Faker::Company.name)
+        visit new_merchant_discount_path(merchant)
+
+        fill_in :quantity, with: "10"
+
+        click_button 'Create Bulk Discount'
+
+        expect(current_path).to eq(new_merchant_discount_path(merchant))
+    
+        expect(page).to have_content 'Error: Please fill in all fields.'
+      end
 end
