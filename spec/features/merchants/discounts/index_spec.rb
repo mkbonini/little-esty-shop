e935f59e3204b704303b439d93fcb5fa796708ad
mkbonini <mkbonini@gmail.com>
2022-08-04 16:17:33 -0600

    test: dicount index has links to show pages

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -10,10 +10,20 @@ RSpec.describe 'Merchant Discounts Index page' do
 
         visit "/merchants/#{merchant_1.id}/discounts"
 
-        within "#disocunt-list" do
+        within "#discount-list" do
             expect(page).to have_link("Threshold Quantity: 10, Discount: 20%")
             expect(page).to have_link("Threshold Quantity: 15, Discount: 30%")
             expect(page).to_not have_content("Threshold Quantity: 20, Discount: 40%")
         end
+    end
+
+    it 'each discount links to discount show page' do
+        merchant_1 = Merchant.create!(name: 'Mike Dao')
+        discount_1 = BulkDiscount.create!(quantity: 10, discount: 20, merchant_id: merchant_1.id)
+
+        visit "/merchants/#{merchant_1.id}/discounts"
+        click_on("Threshold Quantity: 10, Discount: 20%")
+
+        expect(current_path).to eq(merchant_discount_path(merchant_1, discount_1))
     end
 end
