bbe8cc8317d9a9ef726e3f4c406eb4025db81220
mkbonini <mkbonini@gmail.com>
2022-08-04 16:35:34 -0600

    feat: add new discount view and routing with link

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -6,4 +6,21 @@ class MerchantDiscountsController < ApplicationController
     def show
         @discount = BulkDiscount.find(params[:id])
     end
+    
+    def new
+        @merchant = Merchant.find(params[:merchant_id])
+    end
+
+    def create
+        merchant = Merchant.find(params[:merchant_id])
+        merchant.items.new(discount_params)
+    
+        if merchant.save
+          redirect_to merchant_discount_path(merchant)
+          flash[:notice] = "Item successfully created!"
+        else
+          redirect_to new_merchant_discount_path(merchant)
+          flash[:alert] = "Error: Please fill in all fields."
+        end
+    end
 end
