1ec77ba503ea06d0dff661497670515de2a0f21c
mkbonini <mkbonini@gmail.com>
2022-08-08 10:09:48 -0400

    test: edit button on show page

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe 'Merchant Discounts show page' do
             expect(page).to_not have_content("Threshold Quantity: 15")
         end
     end
+
+    it 'has a link to edit discounts' do
+        merchant_1 = Merchant.create!(name: 'Mike Dao')
+        discount_1 = BulkDiscount.create!(quantity: 10, discount: 20, merchant_id: merchant_1.id)
+        discount_2 = BulkDiscount.create!(quantity: 15, discount: 30, merchant_id: merchant_1.id)
+
+        visit "/merchants/#{merchant_1.id}/discounts/#{discount_1.id}"
+
+        expect(page).to have_link("Edit Discount")
+        click_on("Edit Discount")
+        expect(current_path).to eq(edit_merchant_discoutn_path(merchant, discount_1))
+    end
 end
