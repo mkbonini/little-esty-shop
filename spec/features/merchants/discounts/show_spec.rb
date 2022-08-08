8501cf309b0f921962d9fab10c6dc5c543f3d42a
mkbonini <mkbonini@gmail.com>
2022-08-08 10:01:43 -0400

    test: discount show page details

---
+++ spec/features/merchants/discounts/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Discounts show page' do
+    it 'lists all discounts with with quantity threshold and discount' do
+        merchant_1 = Merchant.create!(name: 'Mike Dao')
+        discount_1 = BulkDiscount.create!(quantity: 10, discount: 20, merchant_id: merchant_1.id)
+        discount_2 = BulkDiscount.create!(quantity: 15, discount: 30, merchant_id: merchant_1.id)
+
+        visit "/merchants/#{merchant_1.id}/discounts/#{discount_1.id}"
+
+        within "#discount-details" do
+            expect(page).to have_content("Threshold Quantity: 10")
+            expect(page).to have_content("Discount: 20%")
+            expect(page).to_not have_content("Threshold Quantity: 15")
+        end
+    end
+end
