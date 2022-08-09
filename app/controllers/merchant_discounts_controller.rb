3816c800356d1c3c260f926c5d2e19ac192f1348
mkbonini <mkbonini@gmail.com>
2022-08-09 10:03:32 -0600

    feat: edit discount

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -28,6 +28,17 @@ class MerchantDiscountsController < ApplicationController
       @facade = discount_objects
     end
 
+    def update
+      discount = BulkDiscount.find(params[:id])
+      if discount.update(discount_params)
+        flash[:notice] = "Item successfully updated!"
+        redirect_to merchant_discount_path(discount.merchant, discount)
+      else
+        flash[:alert] = "Error: Please fill in all fields."
+        redirect_to edit_merchant_discount_path(discount.merchant, discount)
+      end
+    end
+
     def destroy
       @discount = BulkDiscount.delete(params[:id])
       redirect_to merchant_discounts_path(params[:merchant_id])
