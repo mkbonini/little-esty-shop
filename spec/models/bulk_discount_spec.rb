25e8ad67cfec7d8f1a2bf24196372249a1d22270
mkbonini <mkbonini@gmail.com>
2022-08-09 13:45:42 -0600

    test: discounted revenue

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe BulkDiscount, type: :model do
   describe 'relationships' do
     it { should belong_to :merchant }
   end
+
+  describe ' instance methods' do
+    it 'gives discounted revenue based on bulk discounts' do
+      merchant = Merchant.create!(name: 'amazon')
+      customer = Customer.create!(first_name: 'Billy', last_name: 'Bob')
+      item_1 = Item.create!(name: 'pet rock', description: 'a rock you pet', unit_price: 10000, merchant_id: merchant.id)
+      item_2 = Item.create!(name: 'ferbie', description: 'monster toy', unit_price: 66600, merchant_id: merchant.id)
+      invoice_1 = Invoice.create!(status: 'completed', customer_id: customer.id)
+      discount_1 = BulkDiscount.create!(quantity: 10, discount: 0.20, merchant_id: merchant.id)
+      discount_2 = BulkDiscount.create!(quantity: 15, discount: 0.30, merchant_id: merchant.id)
+
+
+      InvoiceItem.create!(quantity: 11, unit_price: 100, status: 'shipped', item: item_1, invoice: invoice_1)
+      InvoiceItem.create!(quantity: 16, unit_price: 10, status: 'shipped', item: item_2, invoice: invoice_1)
+
+      expect(invoice_1.discount_revenue).to eq(992)
+    end
+  end
 end
